19ca2af58edef33eecba9b879f58e7e3d11afdfb
Llusx <meta.bit@live.com>
2017-05-08 10:54:23 +0200

    Add ConsumeLootbox Event to Server

---
+++ server-side/src/main/java/GameModel/ServerController/EventListeners/ConsumeLootboxListener.java
@@ -0,0 +1,20 @@
+package GameModel.ServerController.EventListeners;
+
+import GameModel.ServerController.EventObjects.ConsumeLootboxEvent;
+import Mediator.IMediator;
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.listener.DataListener;
+
+/**
+ * Created by latiif on 5/8/17.
+ */
+public class ConsumeLootboxListener extends EventListener implements DataListener<ConsumeLootboxEvent> {
+	public ConsumeLootboxListener(IMediator mediator) {
+		super(mediator);
+	}
+
+	public void onData(SocketIOClient socketIOClient, ConsumeLootboxEvent consumeLootboxEvent, AckRequest ackRequest) throws Exception {
+		mediator.consumeLootbox(consumeLootboxEvent.getId(),consumeLootboxEvent.getGeoPos());
+	}
+}
---
+++ server-side/src/main/java/GameModel/ServerController/EventObjects/ConsumeLootboxEvent.java
@@ -0,0 +1,38 @@
+package GameModel.ServerController.EventObjects;
+
+import GameModel.Player.GeoPos;
+
+/**
+ * Created by latiif on 5/8/17.
+ */
+public class ConsumeLootboxEvent {
+
+	private String id;
+	private GeoPos geoPos;
+
+
+	public ConsumeLootboxEvent(){
+
+	}
+
+	public ConsumeLootboxEvent(String id, GeoPos geoPos) {
+		this.id = id;
+		this.geoPos = geoPos;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public GeoPos getGeoPos() {
+		return geoPos;
+	}
+
+	public void setGeoPos(GeoPos geoPos) {
+		this.geoPos = geoPos;
+	}
+}
