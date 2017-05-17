62d07ac08be5ccfaac0acb7fac708ce2e103d168
Latiif <llusx@outlook.com>
2017-05-17 14:56:53 +0200

    Function decompose exception sending

--- server-side/src/main/java/GameModel/ServerController/Server.java
+++ server-side/src/main/java/GameModel/ServerController/Server.java
@@ -151,11 +151,11 @@ public class Server implements IServer {
 	}
 
 	public void sendException(SocketIOClient socket, Object exception){
-
+		socket.sendEvent("server-side-exception",exception);
 	}
 
 	public void sendException(Object player, Object exception) {
-		map.get(player).sendEvent("server-side-exception",exception);
+		sendException(map.get(player),exception);
 	}
 
 	public <T> JsonArray list2JsonArray(List<T> list){
