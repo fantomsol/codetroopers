d6e50ca9651161b5dbeeacacaa43dea29f55783b
Llusx <meta.bit@live.com>
2017-05-19 21:30:18 +0200

    Adapt IShop and Shop to the new exceptions

--- server-side/src/main/java/GameModel/Shop/IShop.java
+++ server-side/src/main/java/GameModel/Shop/IShop.java
@@ -1,6 +1,9 @@
 package GameModel.Shop;
 
+import GameModel.GameUtils.Exceptions.DuplicateItemException;
+import GameModel.GameUtils.Exceptions.FactoryException;
 import GameModel.GameUtils.Exceptions.GameException;
+import GameModel.GameUtils.Exceptions.InsufficientException;
 import GameModel.Item.Item;
 import GameModel.Player.IPlayer;
 
@@ -10,8 +13,8 @@ import java.util.List;
  * Created by Hugo on 5/1/17.
  */
 public interface IShop {
-    List<Item> getItems() throws GameException;
-    void buyItem(IPlayer IPlayer, Item item) throws GameException;
+    List<Item> getItems() throws FactoryException;
+    void buyItem(IPlayer IPlayer, Item item) throws DuplicateItemException,InsufficientException;
     void sellItem(IPlayer IPlayer, Item item) throws GameException;
-    Item getItem(Integer itemID, String itemType) throws GameException;
+    Item getItem(Integer itemID, String itemType) throws FactoryException;
 }
--- server-side/src/main/java/GameModel/Shop/Shop.java
+++ server-side/src/main/java/GameModel/Shop/Shop.java
@@ -1,6 +1,9 @@
 package GameModel.Shop;
 
+import GameModel.GameUtils.Exceptions.DuplicateItemException;
+import GameModel.GameUtils.Exceptions.FactoryException;
 import GameModel.GameUtils.Exceptions.GameException;
+import GameModel.GameUtils.Exceptions.InsufficientException;
 import GameModel.Item.Armours.ArmoursDirectory;
 import GameModel.Item.Armours.ArmoursFactory;
 import GameModel.Item.Item;
@@ -15,7 +18,7 @@ import java.util.List;
  * Created by hugo on 5/1/17.
  */
 public class Shop implements IShop {
-    public List<Item> getItems() throws GameException {
+    public List<Item> getItems() throws FactoryException {
         List<Item> result =  new ArrayList<Item>();
 
 
@@ -32,7 +35,7 @@ public class Shop implements IShop {
     }
 
 
-    public void buyItem(IPlayer IPlayer, Item item) throws GameException {
+    public void buyItem(IPlayer IPlayer, Item item) throws InsufficientException,DuplicateItemException {
         IPlayer.buyItem(item);
     }
 
@@ -40,7 +43,7 @@ public class Shop implements IShop {
         IPlayer.sellItem(item,Double.valueOf(item.getCost()*ShopConstants.REFUND_PERCENTAGE).intValue());
     }
 
-    public Item getItem(Integer itemID, String itemType) throws GameException {
+    public Item getItem(Integer itemID, String itemType) throws FactoryException {
         if (itemType.equals("armour")){
             return ArmoursFactory.createArmour(itemID);
         }
