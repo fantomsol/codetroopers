135e91377b664df1a0499c464db2a1071dd24d4f
Llusx <meta.bit@live.com>
2017-05-23 16:38:37 +0200

    Remove unneeed dependencies

--- server-side/src/main/java/Mediator/ServerModelMediator.java
+++ server-side/src/main/java/Mediator/ServerModelMediator.java
@@ -1,11 +1,11 @@
 package Mediator;
 
+import com.corundumstudio.socketio.SocketIOClient;
 import com.cth.codetroopers.pixelwars.serverside.GameUtils.Exceptions.GameException;
+import com.cth.codetroopers.pixelwars.serverside.GameUtils.GeoPos;
 import com.cth.codetroopers.pixelwars.serverside.Item.Item;
 import com.cth.codetroopers.pixelwars.serverside.Lootbox.ILootbox;
-import com.cth.codetroopers.pixelwars.serverside.GameUtils.GeoPos;
 import com.cth.codetroopers.pixelwars.serverside.Player.IPlayer;
-import com.corundumstudio.socketio.SocketIOClient;
 import com.cth.codetroopers.pixelwars.serverside.ServerController.IServer;
 import com.cth.codetroopers.pixelwars.serverside.WorldPackage.IWorld;
 
