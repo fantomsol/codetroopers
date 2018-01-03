b32d62cb0bad59e48d38cabe13e8e89dd3e366df
Lumo <lovisa.landgren@gmail.com>
2018-01-03 21:29:18 +0100

    Alter geopos so lat and long can't be negative.

---
+++ server-side/src/main/java/com/cth/codetroopers/pixelwars/serverside/Beings/Avatar/Avatar.java
@@ -0,0 +1,8 @@
+package com.cth.codetroopers.pixelwars.serverside.Beings.Avatar;
+
+/**
+ * Created by lumo on 13/05/17.
+ */
+public enum Avatar {
+	JIM, WILLOW, KARMA, SIBOAN, KYLE, TERRY, LOTUS
+}
