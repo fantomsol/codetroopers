5143a01af08b67add02a4ab0d0fee7e5a615a1a3
Lumo <lovisa.landgren@gmail.com>
2018-01-03 21:41:50 +0100

    Edit test to work as latitude and longitude now can't be negative

--- server-side/src/test/java/GeoDistanceTest.java
+++ server-side/src/test/java/GeoDistanceTest.java
@@ -4,40 +4,42 @@ import com.cth.codetroopers.pixelwars.serverside.GameUtils.GeoPos;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static java.lang.Math.abs;
+
 /**
  * Created by latiif on 3/30/17.
  */
 public class GeoDistanceTest {
 	@Test
-	public void distanceTest(){
+	public void distanceTest() {
 		GeoPos p1;
 		GeoPos p2;
 
-		for (int i=0;i<50;i++){
+		for (int i = 0; i < 50; i++) {
 			try {
-				p1=new GeoPos(Math.random() * 90 + -90,Math.random() * 180 + -180);
-				p2=new GeoPos(Math.random() * 90 + -90,Math.random() * 180 + -180);
+				p1 = new GeoPos(abs(Math.random() * 90 + -90), abs(Math.random() * 180 + -180));
+				p2 = new GeoPos(abs(Math.random() * 90 + -90), abs(Math.random() * 180 + -180));
 
-				Assert.assertEquals(GeoDistance.getDistance(p2,p1), GeoDistance.getDistance(p1,p2));
+				Assert.assertEquals(GeoDistance.getDistance(p2, p1), GeoDistance.getDistance(p1, p2));
 			} catch (GameException e) {
 				//Should not get here
-				Assert.assertTrue(1==2);
+				Assert.assertTrue(1 == 2);
 			}
 
 		}
 	}
 
 	@Test
-	public void invalidGeoPosVariableTest(){
+	public void invalidGeoPosVariableTest() {
 		try {
-			GeoPos point = new GeoPos(888.0,75.0);
+			GeoPos point = new GeoPos(888.0, 75.0);
 		} catch (GameException e) {
 			//Should throw and exception here
 			Assert.assertTrue(true);
 		}
 
 		try {
-			GeoPos point = new GeoPos(-88.0,125.0);
+			GeoPos point = new GeoPos(-88.0, 125.0);
 		} catch (GameException e) {
 			//Should throw and exception here
 			Assert.assertTrue(true);
