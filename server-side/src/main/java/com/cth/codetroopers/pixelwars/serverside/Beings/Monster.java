7130a671ff67e312d0e9debc7b09d9eac5e53114
Lumo <lovisa.landgren@gmail.com>
2018-01-09 12:56:14 +0100

    Add movement method and setter for geopos in monster class.

--- server-side/src/main/java/com/cth/codetroopers/pixelwars/serverside/Beings/Monster.java
+++ server-side/src/main/java/com/cth/codetroopers/pixelwars/serverside/Beings/Monster.java
@@ -1,6 +1,7 @@
 package com.cth.codetroopers.pixelwars.serverside.Beings;
 
 import com.cth.codetroopers.pixelwars.serverside.GameUtils.Exceptions.CombatException;
+import com.cth.codetroopers.pixelwars.serverside.GameUtils.Exceptions.GeographicalException;
 import com.cth.codetroopers.pixelwars.serverside.GameUtils.GeoDistance;
 import com.cth.codetroopers.pixelwars.serverside.GameUtils.GeoPos;
 
@@ -46,6 +47,56 @@ public class Monster extends Being {
 			isAlive = false;
 		}
 	}
+
+	public void move(GeoPos playerPos) throws GeographicalException {
+
+		//Monsters will move in direction of the active player's position with a set speed.
+		double distance = GeoDistance.getDistance(playerPos, this.geoPos);
+
+		int speedvar = 5;
+		double playerLon = playerPos.getLongitude();
+		double playerLat = playerPos.getLongitude();
+		double monsterLat = geoPos.getLatitude();
+		double monsterLon = geoPos.getLongitude();
+
+		if (monsterLat < playerLat){
+			if (monsterLon < playerLon) {
+				setGeoPos(monsterLat + speedvar, monsterLon + speedvar);
+			}
+			if (monsterLon > playerLon){
+				setGeoPos(monsterLat + speedvar, monsterLon - speedvar);
+			}
+		}
+		if (monsterLat > playerLat){
+			if (monsterLon < playerLon) {
+				setGeoPos(monsterLat - speedvar, monsterLon + speedvar);
+			}
+			if (monsterLon > playerLon){
+				setGeoPos(monsterLat - speedvar, monsterLon - speedvar);
+			}
+		}
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	public void setAlive(Boolean alive) {
+		isAlive = alive;
+	}
+
+	public void setGeoPos (double lat, double lon) throws GeographicalException {
+		geoPos = new GeoPos(lat, lon);
+	}
+
 
 	public GeoPos getGeoPos() {
 		return geoPos;
